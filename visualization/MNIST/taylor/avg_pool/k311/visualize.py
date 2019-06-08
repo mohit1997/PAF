131e9841c53bdfe43910065d4ee436015fad802e
mohit1997 <goyal.mohit999@gmail.com>
2019-06-08 20:41:44 +0530

    Visualization Filed Added

---
+++ visualization/MNIST/taylor/avg_pool/k311/visualize.py
@@ -0,0 +1,42 @@
+import numpy as np
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+pca = PCA()
+
+vis_tensor = np.load('vis_tensor.npy')
+labels = np.argmax(np.load('labels.npy'), axis=-1)
+
+sort = np.argsort(labels)
+
+sorted_labels = labels[sort]
+sorted_vistensor = vis_tensor[sort]
+
+print(sorted_labels)
+
+
+pca_ob = pca.fit(sorted_vistensor)
+print(pca_ob.explained_variance_ratio_)
+final_features = pca_ob.transform(sorted_vistensor) 
+print(final_features.shape)
+
+N = len(np.unique(labels))
+# define the colormap
+cmap = plt.cm.jet
+# extract all colors from the .jet map
+cmaplist = [cmap(i) for i in range(cmap.N)]
+# create the new map
+cmap = cmap.from_list('Custom cmap', cmaplist, cmap.N)
+# define the bins and normalize
+bounds = np.linspace(0,N,N+1)
+norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+
+sc = plt.scatter(final_features[:, 0], final_features[:, 1], c=sorted_labels, cmap=cmap, norm=norm)
+plt.colorbar(sc)
+plt.xlabel('PCA Feature 1 with Var_ratio ' + str(pca_ob.explained_variance_ratio_[0]))
+plt.ylabel('PCA Feature 2 with Var_ratio ' + str(pca_ob.explained_variance_ratio_[1]))
+plt.title("Visualizing Ultimate Layer of CNN for Taylor k={3,1,1}")
+plt.tight_layout()
+plt.savefig('vis.png')
+plt.show()
---
+++ visualization/MNIST/taylor_max_k.py
@@ -0,0 +1,212 @@
+from __future__ import print_function
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+def create_variables(name, shape, initializer=tf.initializers.random_normal(), scale=None):
+    '''
+    :param name: A string. The name of the new variable
+    :param shape: A list of dimensions
+    :param initializer: User Xavier as default.
+    :return: The created variable
+    '''
+    
+    ## TODO: to allow different weight decay to fully connected layer and conv layer
+    regularizer = None
+    if scale is not None:
+        regularizer = tf.contrib.layers.l2_regularizer(scale=0.05)
+
+    new_variables = tf.get_variable(name, shape=shape, dtype=tf.float32, initializer=initializer,
+                                    regularizer=regularizer)
+    return new_variables
+
+def bias_variable(shape):
+  """bias_variable generates a bias variable of a given shape."""
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial, name=random_id(10))
+
+
+
+def random_id(length):
+    number = '0123456789'
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    n = len(number)
+    m = len(alpha)
+    id = ''
+    for i in range(0,length,2):
+
+        id += number[np.random.choice(n)]
+        id += alpha[np.random.choice(m)]
+    return id
+
+def update_op(pop_max, max_tensor, m, n, decay):
+    update_max = tf.assign(pop_max, max_tensor*(1-decay) + pop_max*(decay))
+    with tf.control_dependencies([update_max]):
+        max_tensor = m*max_tensor + n
+        return max_tensor
+
+
+def taylor(inp, k, is_train, name="activation", scale=None, decay=0.99):
+    # input_r = tf.reshape(inp, [-1, 1])
+    # shape = inp.get_shape().as_list()
+    # rank = len(shape)
+    # input_r = inp
+    # l = []
+    # for i in range(k):
+    #     temp = tf.pow(input_r, i+1)
+    #     # print(temp)
+    #     l.append(temp)
+
+    # X = tf.stack(l, axis=(rank))
+
+    X = inp
+
+    # with tf.variable_scope("activation", reuse=tf.AUTO_REUSE):
+    #     input_r = tf.layers.batch_normalization(input_r, axis=-1, training=is_train)
+
+    with tf.variable_scope(name, reuse=tf.AUTO_REUSE):
+        # name = random_id(10)
+        m = tf.Variable(2, dtype=tf.float32, name=random_id(10))
+        n = tf.abs(tf.Variable(1e-8, dtype=tf.float32, name="n", trainable=False))
+        print(n)
+
+
+        # Xin = X/max_val
+        if len(X.get_shape()) == 4:
+
+
+            (batch, H, W, C) = X.get_shape()
+            # m = tf.Variable(2*tf.ones([1, 1, 1, C]), dtype=tf.float32, name="m")
+            
+            input_r = X
+            l = []
+            for i in range(k):
+                temp = tf.pow(input_r, i+1)
+                # print(temp)
+                l.append(temp)
+
+            X = tf.stack(l, axis=(rank))
+            Xreshape = X
+            # Xreshape = tf.reshape(X, [-1, H*W*C, 1, 1])
+            max_tensor = tf.reduce_max(Xreshape, axis=[0, 1, 2], keep_dims=True)
+            # max_tensor = tf.reduce_max(max_tensor, axis=0, keep_dims=True)
+            # max_tensor = tf.reduce_max(max_tensor, axis=1, keep_dims=True)
+            min_tensor = tf.reduce_min(Xreshape, axis=[0, 1, 2], keep_dims=True)
+            # min_tensor = tf.reduce_min(min_tensor, axis=0, keep_dims=True)
+            # min_tensor = tf.reduce_min(min_tensor, axis=1, keep_dims=True)
+            max_tensor = tf.abs(tf.maximum(max_tensor, -min_tensor))
+
+            pop_max = tf.Variable(tf.ones([1, 1, 1, C, k]), trainable = False)
+            max_tensor = tf.cond(is_train, lambda: update_op(pop_max, max_tensor, m, n, decay), lambda: m*pop_max + n)
+            Xin = X/max_tensor
+
+            shape = Xin.get_shape().as_list()
+            rank = len(shape)
+            
+
+            # kernel = tf.Variable(np.random.randn(1, 1, k1+k2-1, 1), dtype=tf.float32)
+            kernel = create_variables("activation_weights", [k], scale=scale)
+            # kernel = tf.Variable(tf.random_normal([k1+k2]))
+            # conv = tf.nn.conv2d(out, kernel, [1, 1, 1, 1], padding='SAME')
+
+            # # print(conv.get_shape())
+            # conv = tf.squeeze(conv, [-1])
+            # return(conv)
+            bias = bias_variable([1])
+            conv = tf.reduce_sum(kernel * Xin, axis=-1) + bias
+            return(conv)
+
+
+
+        if len(X.get_shape()) == 3:
+            (batch, W, C) = X.get_shape()
+            # m = tf.Variable(2*tf.ones([1, 1, C]), dtype=tf.float32, name=random_id(10))
+            input_r = X
+            shape = X.get_shape().as_list()
+            rank = len(shape)
+            l = []
+            for i in range(k):
+                temp = tf.pow(input_r, i+1)
+                # print(temp)
+                l.append(temp)
+
+            X = tf.stack(l, axis=(rank))
+            Xreshape = X
+
+            # Xreshape = tf.reshape(X, [-1, W*C, 1])
+            max_tensor = tf.reduce_max(Xreshape, axis=[0, 1], keep_dims=True)
+            # max_tensor = tf.reduce_max(max_tensor, axis=0, keep_dims=True)
+            # max_tensor = tf.reduce_max(max_tensor, axis=1, keep_dims=True)
+            min_tensor = tf.reduce_min(Xreshape, axis=[0, 1], keep_dims=True)
+            # min_tensor = tf.reduce_min(min_tensor, axis=0, keep_dims=True)
+            # min_tensor = tf.reduce_min(min_tensor, axis=1, keep_dims=True)
+            max_tensor = tf.abs(tf.maximum(max_tensor, -min_tensor))
+
+            pop_max = tf.Variable(tf.ones([1, 1, C, k]), trainable = False)
+            max_tensor = tf.cond(is_train, lambda: update_op(pop_max, max_tensor, m, n, decay), lambda: m*pop_max + n)
+            Xin = X/max_tensor
+            
+            
+
+            # kernel = tf.Variable(np.random.randn(1, 1, k1+k2-1, 1), dtype=tf.float32)
+            kernel = create_variables("activation_weights", [k], scale=scale)
+            # kernel = tf.Variable(tf.random_normal([k1+k2]))
+            # conv = tf.nn.conv2d(out, kernel, [1, 1, 1, 1], padding='SAME')
+
+            # # print(conv.get_shape())
+            # conv = tf.squeeze(conv, [-1])
+            # return(conv)
+            bias = bias_variable([1])
+            conv = tf.reduce_sum(kernel * Xin, axis=-1) + bias
+            return(conv)
+
+        if len(X.get_shape()) == 2:
+            (batch, N) = X.get_shape()
+            # m = tf.Variable(2*tf.ones([1, N]), dtype=tf.float32, name=random_id(10))
+            shape = X.get_shape().as_list()
+            rank = len(shape)
+            input_r = X
+            l = []
+            for i in range(k):
+                temp = tf.pow(input_r, i+1)
+                # print(temp)
+                l.append(temp)
+            X = tf.stack(l, axis=(rank))
+            Xreshape = X
+            # Xreshape = tf.reshape(X, [-1, N])
+            max_tensor = tf.reduce_max(Xreshape, axis=[0], keep_dims=True)
+            # max_tensor = tf.reduce_max(max_tensor, axis=0, keep_dims=True)
+            # max_tensor = tf.reduce_max(max_tensor, axis=1, keep_dims=True)
+            min_tensor = tf.reduce_min(Xreshape, axis=[0], keep_dims=True)
+            # min_tensor = tf.reduce_min(min_tensor, axis=0, keep_dims=True)
+            # min_tensor = tf.reduce_min(min_tensor, axis=1, keep_dims=True)
+            max_tensor = tf.abs(tf.maximum(max_tensor, -min_tensor))
+
+            pop_max = tf.Variable(tf.ones([1, N, k]), trainable = False)
+
+            max_tensor = tf.cond(is_train, lambda: update_op(pop_max, max_tensor, m, n, decay), lambda: m*pop_max + n)
+            Xin = X/max_tensor
+
+            
+
+            
+            kernel = create_variables("activation_weights", [k], scale=scale)
+            # kernel = tf.Variable(tf.random_normal([k1+k2]))
+            # conv = tf.nn.conv2d(out, kernel, [1, 1, 1, 1], padding='SAME')
+
+            # # print(conv.get_shape())
+            # conv = tf.squeeze(conv, [-1])
+            # return(conv)
+            bias = bias_variable([1])
+            conv = tf.reduce_sum(kernel * Xin, axis=-1) + bias
+            return(conv)
+
+
+    # size = 1
+    # for i in range(rank-1):
+    #     size = size*shape[i+1]
+    # input_temp = tf.reshape(input_r, [-1, size*(k+1)])
+    # print(input_temp)
+    # _, _, out = add_layer1(input_temp, k+1)
+
+    return input_r
